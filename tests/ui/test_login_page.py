3c53f70b80804ad9963aa6c3ba14f44745554c40
anuranjan7892 <anuranjanprakash007@gmail.com>
2025-04-19 13:07:52 +0530

    added steps in test_login_functionality

--- tests/ui/test_login_page.py
+++ tests/ui/test_login_page.py
@@ -1,11 +1,23 @@
 import pytest
 
+from pageObjects.landingPage import LandingPage
+from pageObjects.loginPage import LoginPage
+from tests.baseTest import BaseTest
 
-class TestLoginPage:
+
+class TestLoginPage(BaseTest):
 
     @pytest.mark.smoke
     def test_login_functionality(self):
         print("User should be able to login successfully")
+        login_page = LoginPage(self.driver)
+        landing_page = LandingPage(self.driver)
+
+        login_page.enter_username("standard_user")
+        login_page.enter_password("secret_sauce")
+        login_page.click_login()
+
+        assert landing_page.is_logo_present(), "Login unsuccessful"
 
     @pytest.mark.smoke
     def test_logout_functionality(self):
